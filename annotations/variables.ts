ca29d3e8155f1b9aeb41ee16bd1e0edcb8ccf86b
Simone <startae14@gmail.com>
2021-08-11 00:08:14 +0200

    07: Object Literal and Arrays Type Annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,21 @@ let nothing: undefined = undefined;
 
 // built in Object
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object Literale
+let point: { x: number, y: number } = {
+  x: 10,
+  y: 20
+};
