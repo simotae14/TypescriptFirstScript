f5f1f1b7f5010a727dbc3bbf46afed2c1206739e
Simone <startae14@gmail.com>
2021-08-26 17:44:24 +0200

    21: Arrays Inference and Type Annotations for accessing values, utilities methods etc...

--- arrays.ts
+++ arrays.ts
@@ -17,3 +17,12 @@ const carsByMake = [
 
 // with empty initialization
 const carsByMakeEmpty: string[][] = [];
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map' and other arrays methods
+carMakers.map((car: string): string => car.toUpperCase());
