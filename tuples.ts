12b855cb2c483cbbac369f76dda8684281c6a192
Simone <startae14@gmail.com>
2021-08-26 18:19:08 +0200

    24: better use Objects than Tuples

--- tuples.ts
+++ tuples.ts
@@ -14,3 +14,10 @@ type Drink = [string, boolean, number];
 const cola: Drink = [ 'brown', true, 30 ];
 const sprite: Drink = ['clear', true, 20];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
