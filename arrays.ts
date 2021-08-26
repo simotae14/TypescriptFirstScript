739ea58509cdd350f77fe92d81c7df56c10e330b
Simone <startae14@gmail.com>
2021-08-26 17:20:54 +0200

    20: Define Annotations and Type Inference with Arrays

---
+++ arrays.ts
@@ -0,0 +1,19 @@
+// with Type Inference
+const carMakers = ['ford', 'toyota', 'chevy'];
+
+// with Explicit annotations
+const carMakers2: string[] = ['opel', 'fiat'];
+
+const carMakers3: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+// two dimentional arrays
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+
+// with empty initialization
+const carsByMakeEmpty: string[][] = [];
