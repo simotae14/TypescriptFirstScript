e2aff9861ee3c7591b4978c3f64db2a01296d07d
Simone <startae14@gmail.com>
2021-09-05 20:37:01 +0200

    33: pties with modifiers inside Classes

--- classes.ts
+++ classes.ts
@@ -1,23 +1,25 @@
 class Vehicle {
+  constructor(public color: string) {
+  }
   protected honk(): void {
     console.log('beep');
   }
 }
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log('vroom');
-  }
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom');
+//   }
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const vehicle = new Vehicle();
-vehicle.honk(); // give error
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
 // create instance of car
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
+
