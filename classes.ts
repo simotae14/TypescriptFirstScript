6c488f4e06424527200557851ab9f5c4a465bf98
Simone <startae14@gmail.com>
2021-09-05 21:32:14 +0200

    34: inheritance and pties of Classes

--- classes.ts
+++ classes.ts
@@ -6,20 +6,23 @@ class Vehicle {
   }
 }
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom');
-//   }
-//   startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log('vroom');
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
 // create instance of car
-// const car = new Car();
-// car.startDrivingProcess();
+const car = new Car(4, 'red');
+car.startDrivingProcess();
 
